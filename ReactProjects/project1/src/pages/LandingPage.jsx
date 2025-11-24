2d4888f9e88f86527ce32a7ceadc4592c89b6ef5
uysolanki <uysolanki@gmail.com>
2025-11-24 19:14:25 +0530

    landing page

--- ReactProjects/project1/src/pages/LandingPage.jsx
+++ ReactProjects/project1/src/pages/LandingPage.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import './LandingPage.css'
+import hand from '/public/myimages/hand.png';
+import right_arrow from "/public/myimages/right-arrow.png";
+import hero_img from "/public/myimages/hero_img1.webp";
+import { Link } from 'react-router-dom'
 const LandingPage = () => {
   return (
-    <>
-        <h1>Welcome to My Shopping Website</h1>
-    </>
-  )
+        <div className="hero">
+
+            <div className="hero-left">
+                <h2>New Arrivals</h2>
+
+                <div className="hero-collection">
+                    <div className="hero-hand-icon">
+                        <p>new</p>
+                        <img src={hand} alt="hand icon" />
+                    </div>
+                    <p>Collections</p>
+                    <p>For everyone</p>
+                </div>
+
+                <Link to="/shop"> <div className="hero-latest-btn" style={{color:'white'}}>
+                    <div>Latest Collection</div>
+                    <img src={right_arrow} alt="arrow" />
+                </div></Link>
+            </div>
+            <div className="hero-right">
+                <img src={hero_img} alt="Hero Image" />
+            </div>
+        </div>
+    );
 }
 
 export default LandingPage
